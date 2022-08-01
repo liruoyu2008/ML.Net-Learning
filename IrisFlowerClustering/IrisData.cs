693e42657281e652bb9b37f17c383d33d2818dbf
Ryu <liruoyu2008@users.noreply.github.com>
2022-08-01 17:20:04 +0800

    1.回归预测 2.聚类

---
+++ IrisFlowerClustering/IrisData.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IrisFlowerClustering
+{
+    public class IrisData
+    {
+        [LoadColumn(0)]
+        public float SepalLength;
+
+        [LoadColumn(1)]
+        public float SepalWidth;
+
+        [LoadColumn(2)]
+        public float PetalLength;
+
+        [LoadColumn(3)]
+        public float PetalWidth;
+    }
+
+    public class ClusterPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public uint PredictedClusterId;
+
+        [ColumnName("Score")]
+        public float[] Distances;
+    }
+}
---
+++ IrisFlowerClustering/TestIrisData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IrisFlowerClustering
+{
+    static class TestIrisData
+    {
+        internal static readonly IrisData Setosa = new IrisData
+        {
+            SepalLength = 7.7f,
+            SepalWidth = 2.6f,
+            PetalLength = 6.9f,
+            PetalWidth = 2.3f
+        };
+    }
+}
