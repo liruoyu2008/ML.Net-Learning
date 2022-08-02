ee78c0e47a8a90431aa15ca580bc85be0072bb20
Ryu <liruoyu2008@users.noreply.github.com>
2022-08-02 17:23:56 +0800

    1.矩阵分解（推荐算法） 2.迁移学习（图像分类）

---
+++ DeepLearning_ImageClassification_Binary/ItemData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepLearning_ImageClassification_Binary
+{
+    class ImageData
+    {
+        public string ImagePath { get; set; }
+
+        public string Label { get; set; }
+    }
+
+    class ModelInput
+    {
+        public byte[] Image { get; set; }
+
+        public UInt32 LabelAsKey { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public string Label { get; set; }
+    }
+
+    class ModelOutput
+    {
+        public string ImagePath { get; set; }
+
+        public string Label { get; set; }
+
+        public string PredictedLabel { get; set; }
+    }
+}
---
+++ DeepLearning_ImageClassification_Binary/Program.cs
@@ -0,0 +1,117 @@
+﻿using DeepLearning_ImageClassification_Binary;
+using Microsoft.ML;
+using Microsoft.ML.Vision;
+using static Microsoft.ML.DataOperationsCatalog;
+
+var projectDirectory = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../../../"));
+var assetsRelativePath = Path.Combine(projectDirectory, "Data");
+
+MLContext mlContext = new MLContext();
+
+// 载入数据并打乱
+IEnumerable<ImageData> images = LoadImagesFromDirectory(folder: assetsRelativePath, useFolderNameAsLabel: true);
+IDataView imageData = mlContext.Data.LoadFromEnumerable(images);
+IDataView shuffledData = mlContext.Data.ShuffleRows(imageData);
+
+// 预处理：字段名称转换为值类型、名称映射、实际数据载入等
+// 猜测：fit方法的使用相当于使处理链实际生效，链条一般仅在最后使用fit实际生效，也可分多段生效，效果一致。
+var preprocessingPipeline = mlContext.Transforms.Conversion.MapValueToKey(
+        outputColumnName: "LabelAsKey",
+        inputColumnName: "Label")
+    .Append(mlContext.Transforms.LoadRawImageBytes(
+        outputColumnName: "Image",
+        imageFolder: assetsRelativePath,
+        inputColumnName: "ImagePath"));
+IDataView preProcessedData = preprocessingPipeline
+                    .Fit(shuffledData)
+                    .Transform(shuffledData);
+
+// 将经过预处理的数据按 7:2:1 分为训练、验证和测试数据
+TrainTestData trainSplit = mlContext.Data.TrainTestSplit(data: preProcessedData, testFraction: 0.3);
+TrainTestData validationTestSplit = mlContext.Data.TrainTestSplit(trainSplit.TestSet);
+IDataView trainSet = trainSplit.TrainSet;
+IDataView validationSet = validationTestSplit.TrainSet;
+IDataView testSet = validationTestSplit.TestSet;
+
+// 图像训练的几个参数
+var classifierOptions = new ImageClassificationTrainer.Options()
+{
+    FeatureColumnName = "Image",
+    LabelColumnName = "LabelAsKey",
+    ValidationSet = validationSet,
+    Arch = ImageClassificationTrainer.Architecture.ResnetV250,
+    MetricsCallback = (metrics) => Console.WriteLine(metrics),
+    TestOnTrainSet = false,
+    ReuseTrainSetBottleneckCachedValues = true,
+    ReuseValidationSetBottleneckCachedValues = true
+};
+
+// 训练
+var trainingPipeline = mlContext.MulticlassClassification.Trainers.ImageClassification(classifierOptions)
+    .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+ITransformer trainedModel = trainingPipeline.Fit(trainSet);
+
+// 分类单个图像
+ClassifySingleImage(mlContext, testSet, trainedModel);
+
+// 分类批量图像
+ClassifyImages(mlContext, testSet, trainedModel);
+
+
+IEnumerable<ImageData> LoadImagesFromDirectory(string folder, bool useFolderNameAsLabel = true)
+{
+    var files = Directory.GetFiles(folder, "*", searchOption: SearchOption.AllDirectories);
+    foreach (var file in files)
+    {
+        if ((Path.GetExtension(file) != ".jpg") && (Path.GetExtension(file) != ".png"))
+            continue;
+
+        var label = Path.GetFileName(file);
+
+        if (useFolderNameAsLabel)
+            label = Directory.GetParent(file).Name;
+        else
+        {
+            for (int index = 0; index < label.Length; index++)
+            {
+                if (!char.IsLetter(label[index]))
+                {
+                    label = label.Substring(0, index);
+                    break;
+                }
+            }
+        }
+
+        yield return new ImageData()
+        {
+            ImagePath = file,
+            Label = label
+        };
+    }
+}
+
+void ClassifySingleImage(MLContext mlContext, IDataView data, ITransformer trainedModel)
+{
+    PredictionEngine<ModelInput, ModelOutput> predictionEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(trainedModel);
+    ModelInput image = mlContext.Data.CreateEnumerable<ModelInput>(data, reuseRowObject: true).First();
+    ModelOutput prediction = predictionEngine.Predict(image);
+    Console.WriteLine("Classifying single image");
+    OutputPrediction(prediction);
+}
+
+void OutputPrediction(ModelOutput prediction)
+{
+    string imageName = Path.GetFileName(prediction.ImagePath);
+    Console.WriteLine($"Image: {imageName} | Actual Value: {prediction.Label} | Predicted Value: {prediction.PredictedLabel}");
+}
+
+void ClassifyImages(MLContext mlContext, IDataView data, ITransformer trainedModel)
+{
+    IDataView predictionData = trainedModel.Transform(data);
+    IEnumerable<ModelOutput> predictions = mlContext.Data.CreateEnumerable<ModelOutput>(predictionData, reuseRowObject: true).Take(10);
+    Console.WriteLine("Classifying multiple images");
+    foreach (var prediction in predictions)
+    {
+        OutputPrediction(prediction);
+    }
+}
