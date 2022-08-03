22547e4c9f9a6d270d5ab15e6f281d7d6b041c1a
Ryu <liruoyu2008@users.noreply.github.com>
2022-08-03 16:05:05 +0800

    1.图像分类（深度学习）

---
+++ TransferLearningTF/ImageData.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.ML.Data;
+
+namespace TransferLearningTF
+{
+    public class ImageData
+    {
+        [LoadColumn(0)]
+        public string ImagePath;
+
+        [LoadColumn(1)]
+        public string Label;
+    }
+
+    public class ImagePrediction : ImageData
+    {
+        public float[] Score;
+
+        public string PredictedLabelValue;
+    }
+
+    struct InceptionSettings
+    {
+        public const int ImageHeight = 224;
+        public const int ImageWidth = 224;
+        public const float Mean = 117;
+        public const float Scale = 1;
+        public const bool ChannelsLast = true;
+    }
+}
